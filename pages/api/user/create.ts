dd6ddb5fc968bf7bbb04447894a8d120d77cd10e
Max Leiter <mleiter@usc.edu>
2020-08-01 20:51:58 -0700

    user/create updates user if exists

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -1,4 +1,4 @@
-import { database } from '@/lib';
+import { admin, database } from '@/lib';
 
 /*
  * /api/user/create?displayName={displayName}&location={location}
@@ -8,7 +8,28 @@ import { database } from '@/lib';
 export default async (req, res) => {
     try {
         let { displayName, latitude, longitude, playerId } = req.query;
+        let update = false;
+        admin
+            .database()
+            .ref('user/' + playerId)
+            .on('value', (snapshot) => {
+                if (snapshot.exists()) {
+                    update = true;
+                }
+            });
 
+        // user already exists, so they're re-signing in. Update their location and display name
+        if (update) {
+            var updates = {};
+            updates['/user/' + playerId + '/location/longitude'] = longitude;
+            updates['/user/' + playerId + '/location/latitude'] = latitude;
+            updates['/user/' + playerId + '/displayName'] = displayName;
+
+            // * Update
+            database.update(updates);
+            res.status(200).json(updates);
+            return;
+        }
 
         // * New Game Entry
         let user = {
